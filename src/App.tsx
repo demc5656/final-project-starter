035dbb01e09d891ccab2cee3f27b43bd779a5aad
DM <mcnally@udel.edu>
2021-10-10 16:20:58 -0400

    idk what i changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
+import Button from "react-bootstrap/Button";
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-          
+
                 </p>
+                
+                <button>Delete</button>
           Welcome!
             </header>
         </div>
